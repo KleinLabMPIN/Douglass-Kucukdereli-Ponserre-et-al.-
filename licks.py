72bb32e0dc66296b69957569c2ca1642447e0ae9
Hakan Kucukdereli <hkucukdereli@gmail.com>
2016-10-06 14:19:11 +0200

    final version

--- licks.py
+++ licks.py
@@ -15,13 +15,6 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.ndimage.filters import gaussian_filter
-
-curDirPath =  os.path.dirname(os.path.realpath(__file__))
-os.chdir(curDirPath)
-print "Working directory is set to ", os.getcwd()
-#fileName = "data\Kissykissylicklick\mCherry\\6910_test_adlib.csv"
-fileName = "data\Kissykissylicklick\mCherry\\6862_test_adlib.csv"
 
 def getLicks(fileName, CH, th_low, th_high, save= False):
     lickData = pd.read_csv(fileName, sep= ' ', header= 0, error_bad_lines= False)
@@ -29,7 +22,8 @@ def getLicks(fileName, CH, th_low, th_high, save= False):
     lickData = lickData[lickData.CH0 <= 255]
     lickData = lickData[lickData.CH0 > -1]
     lickData = lickData[np.isfinite(lickData['TimeStamp']) & lickData['TimeStamp'] < lickData.TimeStamp.loc[-1:]]
-    #lickData = lickData.reindex(np.arange(0, len(lickData.TimeStamp)))
+    lickData.index = np.arange(0, len(lickData.TimeStamp))
+    lickData = lickData[lickData.TimeStamp[lickData.index] > lickData.TimeStamp[lickData.index-1]]
     lickData.index = np.arange(0, len(lickData.TimeStamp))
 
     #print(lickData.CH0)
@@ -55,9 +49,15 @@ def getLicks(fileName, CH, th_low, th_high, save= False):
 
     lickTimes = pd.DataFrame({'Start' : np.array(lickDigi.Start[np.isfinite(lickData['Start'])]), 'End' : np.array(lickDigi.End[np.isfinite(lickData['End'])])})
     lickTimes['Duration'] = lickTimes.End - lickTimes.Start
+    lickTimes['Count'] = pd.DataFrame(np.zeros(len(lickTimes.Duration)))
 
     # filter for fast licks
     lickTimes = lickTimes[lickTimes.Duration > th_low]
+    lickTimes.Count[lickTimes.Duration > th_low] = 1
+
+    # extrapolate the long licks
+    freq = 12 #Hz
+    lickTimes.Count[lickTimes.Duration > th_high] = lickTimes.Duration[lickTimes.Duration > th_high] / 80
 
     if(save):
         lickDigi.to_csv(fileName + "_.csv", sep= ',')
@@ -66,16 +66,26 @@ def getLicks(fileName, CH, th_low, th_high, save= False):
 
     return lickDigi, lickTimes
 
-th_low= 6.0
-th_high= 200000.0
+if __name__ == "__main__":
 
-dirName = '.\data\Kissykissylicklick\NpHR\\'
-files = os.listdir(dirName)
-for file in files:
-    lickDigi, lickTimes = getLicks(dirName+file, CH = 0, th_low= th_low, th_high= th_high)
+    curDirPath =  os.path.dirname(os.path.realpath(__file__))
+    os.chdir(curDirPath)
+    print "Working directory is set to ", os.getcwd()
 
-    #lickDigi.to_csv("data\lickdata.csv", sep= ',')
+    th_low= 6.0
+    th_high= 140.0
 
-    lickTimes.Duration[lickTimes.Duration < th_high].hist(bins= 100)
-    #lickTimes.Start[lickTimes.Duration < threshold_high].hist(bins= 100)
-    plt.show()
+    dirName = '.\data\Kissykissylicklick\NpHR\\'
+    #fileName = "data\Kissykissylicklick\mCherry\\6862_test_adlib.csv"
+
+    files = os.listdir(dirName)
+    for file in files:
+        lickDigi, lickTimes = getLicks(dirName+file, CH = 0, th_low= th_low, th_high= th_high)
+
+        #print(lickTimes[550:600])
+
+        #lickDigi.to_csv("data\lickdata.csv", sep= ',')
+        print(max(lickTimes.Duration))
+        #lickTimes.Duration[lickTimes.Duration < th_high].hist(bins= max(lickTimes.Duration)/10)
+        #lickTimes.Start[lickTimes.Duration < th_high].hist(bins= 100)
+        plt.show()
